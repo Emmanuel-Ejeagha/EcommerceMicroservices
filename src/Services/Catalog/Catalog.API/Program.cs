c99316d37a3f5fae8581e3611d607bcf89a7ba85
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-11 17:15:43 +0100

    feat(api): configure Carter endpoints and Marten database in Program.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,16 @@
+using Carter;
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 
 app.Run();
