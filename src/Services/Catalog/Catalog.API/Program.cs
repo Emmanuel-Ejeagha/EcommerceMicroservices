c1a31b52289b52351c3ff6746855a312ca76c18b
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-12 17:03:23 +0100

    feat(api): configure MediatR validation pipeline and register validators

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,14 +1,24 @@
-using Carter;
-using Marten;
+using BuildingBlocks.Behaviors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
 builder.Services.AddCarter();
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+
 var app = builder.Build();
 
 app.MapCarter();
