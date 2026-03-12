1347d017d3575bed79d1d77c6a059d871ec874a2
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-12 16:55:20 +0100

    feat(products): implements UpdateProduct handler and validator

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdatProductValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public class UpdatProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdatProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required.");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required.")
+            .MaximumLength(100).WithMessage("Product name must not exceed 100 characters.");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image file is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero.");
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,6 +1,4 @@
-﻿using ImTools;
-
-namespace Catalog.API.Products.UpdateProduct;
+﻿namespace Catalog.API.Products.UpdateProduct;
 
 public record UpdateProductCommand(
     Guid Id,
