a4c9632411d4eb2fbe8ac7b0fb02fcb3fc3cb9ad
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-12 17:02:29 +0100

    feat(products): add CreateProductValidator validator

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required.")
+            .MaximumLength(100).WithMessage("Product name must not exceed 100 characters.");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image file is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero.");
+    }
+}
