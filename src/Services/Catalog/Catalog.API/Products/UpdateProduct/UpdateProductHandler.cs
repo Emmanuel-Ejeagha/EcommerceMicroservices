ac2f1cea3ef7665e818baf4ca8f52340a4d6d2ac
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-13 14:55:08 +0100

    refactor(products): remove ILogger dependency from UpdateProductHandler

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -9,20 +9,14 @@ public record UpdateProductCommand(
     decimal Price) : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
 public class UpdateProductCommandHandler
-    (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+    (IDocumentSession session)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
     {
-        logger.LogInformation("UpdateProductCommand.Handle called with {@Command}", command);
-
         var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
         if (product == null)
-<<<<<<< HEAD
             throw new ProductNotFoundException(command.Id);
-=======
-            throw new ProductNotFoundException();
->>>>>>> ec6ee386cc153686daf03f0f4ba2ecbad6e83045
 
         product.Name = command.Name;
         product.Category = command.Category;
