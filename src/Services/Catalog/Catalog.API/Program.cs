8ea44d725dbb5fab58925c36f6b5ecb246926f0b
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-13 14:52:55 +0100

    feat(api): integrate LoggingBehavior and CustomExceptionHandler with MediatR pipeline

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-using BuildingBlocks.Exceptions.Handler;
-=======
-using BuildingBlocks.Behaviors;
->>>>>>> ec6ee386cc153686daf03f0f4ba2ecbad6e83045
-
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
@@ -12,6 +6,7 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 
@@ -22,18 +17,12 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
-<<<<<<< HEAD
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-=======
->>>>>>> ec6ee386cc153686daf03f0f4ba2ecbad6e83045
 
 var app = builder.Build();
 
 app.MapCarter();
 
-<<<<<<< HEAD
 app.UseExceptionHandler(options => { });
 
-=======
->>>>>>> ec6ee386cc153686daf03f0f4ba2ecbad6e83045
 app.Run();
