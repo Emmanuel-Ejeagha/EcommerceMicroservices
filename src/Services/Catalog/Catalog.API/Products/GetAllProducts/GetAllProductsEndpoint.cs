62b6bf6500c9e9cfa6988b8b66dca7e73afba509
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-14 15:41:13 +0100

    feat(products): add pagination to GetAllProducts endpoint and handler

--- src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductsEndpoint.cs
@@ -1,14 +1,17 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 public record GetProductsResponse(IEnumerable<ProductDto> Products);
 
-public class GetProductsEndpoint : ICarterModule
+public class GetAllProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender, CancellationToken cancellationToken = default) =>
+        app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender, CancellationToken cancellationToken = default) =>
         {
-            var response = await sender.Send(new GetProductsQuery(), cancellationToken);
+            var query = request.Adapt<GetProductsQuery>();
+
+            var response = await sender.Send(query, cancellationToken);
 
             var result = new GetProductsResponse(response.Products);
 
--- src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductsHandler.cs
@@ -1,18 +1,19 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<ProductDto> Products);
 
-internal class GetProductsQueryHandler
-    (IDocumentSession session, ILogger<GetProductsQueryHandler> logger) 
+internal class GetAllProductsQueryHandler
+    (IDocumentSession session, ILogger<GetAllProductsQueryHandler> logger) 
     : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
         logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
 
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>()
+            .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
         var productDtos = products.Adapt<IEnumerable<ProductDto>>();
 
