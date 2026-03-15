c8f801337fceb7a4a6d759ae262ae89b6a76882e
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-15 13:08:36 +0100

    refactor(catalog): update global usings to include Marten pagination

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -2,10 +2,12 @@
 global using Mapster;
 global using MediatR;
 global using Marten;
+global using FluentValidation;
 global using Catalog.API.DTOs;
-global using BuildingBlocks.CQRS;
+global using Catalog.API.Data;
+global using Marten.Pagination;
 global using Catalog.API.Models;
+global using BuildingBlocks.CQRS;
 global using Catalog.API.Exceptions;
 global using BuildingBlocks.Behaviors;
-global using FluentValidation;
 global using BuildingBlocks.Exceptions.Handler;
