a7f37fe72118a9a0f567598d5e6659bf9ca3c8f8
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-16 16:39:33 +0100

    feat(basket): implement GetBasket endpoint and handler

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+//public record GetBasketRequest(string UserName);
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}",
+            async (string userName, ISender sender, CancellationToken cancellationToken = default) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName), cancellationToken);
+
+            var response = result.Adapt<GetBasketResponse>(); 
+
+            return Results.Ok(response);
+        })
+        .WithName("Get Basket By Username")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get the shopping cart for a specific user.")
+        .WithDescription("Retrieves the shopping cart associated with the provided username. " +
+        "   If the user does not have a shopping cart, an empty cart will be returned.");
+    }
+}
