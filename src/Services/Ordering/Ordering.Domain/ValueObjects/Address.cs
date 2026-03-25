486d368d8e811d58ee82e54b7bf9f9f77fdc5327
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-25 16:59:55 +0100

    feat(ordering): add value objects for order domain (Address, IDs, Payment, OrderName)

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,41 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Address
+{
+    public string FirstName { get; } = default!;
+    public string LastName { get; } = default!;
+    public string? EmailAddress { get; } = default!;
+    public string AddressLine { get; } = default!;
+    public string Country { get; } = default!;
+    public string State { get; } = default!;
+    public string PostalCode { get; } = default!;
+
+    protected Address()
+    {
+    }
+
+    public Address(
+        string firstname, string lastname, string emailAddress, string addressline,
+        string country, string state, string postalCode)
+    {
+        FirstName = firstname;
+        LastName = lastname;
+        EmailAddress = emailAddress;
+        AddressLine = addressline;
+        Country = country;
+        State = state;
+        PostalCode = postalCode;
+    }
+
+    public static Address Of(
+        string firstname, string lastname, string emailAddress, string addressline,
+        string country, string state, string postalCode)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(firstname);
+        ArgumentException.ThrowIfNullOrWhiteSpace(lastname);
+        ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+        ArgumentException.ThrowIfNullOrWhiteSpace(addressline);
+
+        return new Address(firstname, lastname, emailAddress, addressline, country, state, postalCode);
+    }
+}
