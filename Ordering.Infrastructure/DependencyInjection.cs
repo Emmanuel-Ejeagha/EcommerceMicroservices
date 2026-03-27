8c4adf0e6d1c8fe85127dc7e07ed8db4bdeb4fce
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 11:31:03 +0100

    feat(ordering): add dependency injection setup for infrastructure layer

---
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<AppDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetService<ISaveChangesInterceptor>()!);
+            options.UseSqlServer(connectionString);
+        });
+
+        return services;
+    }
+}
