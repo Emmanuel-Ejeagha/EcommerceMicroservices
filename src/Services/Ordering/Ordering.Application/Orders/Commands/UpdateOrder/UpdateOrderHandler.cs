e90d8713ef7ebaab5a4a3b205933aa90fa845183
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 19:25:54 +0100

    feat(ordering): add UpdateOrder command, handler and validator

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
@@ -0,0 +1,63 @@
+﻿namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderHandler(IAppDbContext dbContext)
+    : ICommandHandler<UpdateOrderCommand, UpdateOrderResult>
+{
+    public async Task<UpdateOrderResult> Handle(UpdateOrderCommand command, CancellationToken cancellationToken)
+    {
+        // Update order entity from command object
+        //save to database
+        //return UpdateOrderResult
+
+        var orderId = OrderId.Of(command.Order.Id);
+        var order = await dbContext.Orders
+            .FindAsync([orderId], cancellationToken: cancellationToken);
+
+        if (order == null) 
+            throw new OrderNotFoundException(command.Order.Id);
+
+        UpdateOrderWithNewValue(order, command.Order);
+
+        dbContext.Orders.Update(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateOrderResult(true);
+    }
+
+    private void UpdateOrderWithNewValue(Order order, OrderDto orderDto)
+    {
+        var updatedShippingAddress = Address.Of(
+            orderDto.ShippingAddress.FirstName,
+            orderDto.ShippingAddress.LastName,
+            orderDto.ShippingAddress.EmailAddress,
+            orderDto.ShippingAddress.AddressLine,
+            orderDto.ShippingAddress.Country,
+            orderDto.ShippingAddress.State,
+            orderDto.ShippingAddress.City,
+            orderDto.ShippingAddress.PostalCode);
+
+        var updatedBillingAddress = Address.Of(
+            orderDto.BillingAddress.FirstName,
+            orderDto.BillingAddress.LastName,
+            orderDto.BillingAddress.EmailAddress,
+            orderDto.BillingAddress.AddressLine,
+            orderDto.BillingAddress.Country,
+            orderDto.BillingAddress.State,
+            orderDto.BillingAddress.City,
+            orderDto.BillingAddress.PostalCode);
+
+        var updatedPayment = Payment.Of(
+                orderDto.Payment.CardName,
+                orderDto.Payment.CardName,
+                orderDto.Payment.Expiration,
+                orderDto.Payment.Cvv,
+                orderDto.Payment.PaymentMethod);
+
+        order.Update(
+            orderName: OrderName.Of(orderDto.OrderName),
+            shippingAddress: updatedShippingAddress,
+            billingAddress: updatedBillingAddress,
+            payment: updatedPayment,
+            status: orderDto.Status);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderValidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderValidator()
+    {
+        RuleFor(x => x.Order.Id).NotEmpty().WithMessage("Id is required");
+        RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("CustomerId is required");
+    }
+}
