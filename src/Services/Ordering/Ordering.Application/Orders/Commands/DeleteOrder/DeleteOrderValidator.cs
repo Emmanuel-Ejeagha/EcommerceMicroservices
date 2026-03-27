94a8b0532488b18135b002fde2a3715c509a4aa1
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 19:25:24 +0100

    feat(ordering): add DeleteOrder command, handler and validator

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderValidator()
+    {
+        RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required");
+    }
+}
