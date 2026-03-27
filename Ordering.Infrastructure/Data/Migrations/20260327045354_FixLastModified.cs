bf74882c157899534ab7cd3e1dab6944ce06f228
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 06:59:56 +0100

    feat(ordering): add initial database migrations

---
+++ Ordering.Infrastructure/Data/Migrations/20260327045354_FixLastModified.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixLastModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LasteModified",
+                table: "Products",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "LasteModified",
+                table: "Orders",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "LasteModified",
+                table: "OrderItems",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "LasteModified",
+                table: "Customers",
+                newName: "LastModified");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                table: "Products",
+                newName: "LasteModified");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                table: "Orders",
+                newName: "LasteModified");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                table: "OrderItems",
+                newName: "LasteModified");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                table: "Customers",
+                newName: "LasteModified");
+        }
+    }
+}
