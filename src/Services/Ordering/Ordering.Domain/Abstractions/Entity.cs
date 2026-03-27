80cf6b49748de5ce11f4eee909c153d176bd43b3
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 06:45:40 +0100

    feat(ordering): fix lastmodifed property spelling

--- src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -5,6 +5,6 @@ public abstract class Entity<T> : IEntity<T>
     public T Id { get; set; }
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
-    public DateTime? LasteModified { get; set; }
+    public DateTime? LastModified { get; set; }
     public string? LastModifiedBy { get; set; }
 }
