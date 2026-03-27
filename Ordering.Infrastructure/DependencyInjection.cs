28578b7c4597cd38cf15b743eb77b7a3bb096459
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 19:32:52 +0100

    chore(ordering): configure DI, global usings and update Application.csproj

--- Ordering.Infrastructure/DependencyInjection.cs
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 using Ordering.Infrastructure.Data;
 
 namespace Ordering.Infrastructure;
@@ -20,6 +21,8 @@ public static class DependencyInjection
             options.AddInterceptors(sp.GetService<ISaveChangesInterceptor>()!);
             options.UseSqlServer(connectionString);
         });
+
+        services.AddScoped<IAppDbContext, AppDbContext>();
 
         return services;
     }
