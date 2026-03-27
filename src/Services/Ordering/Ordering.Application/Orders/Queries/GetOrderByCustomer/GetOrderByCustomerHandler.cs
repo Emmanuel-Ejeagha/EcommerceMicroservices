60158ff3dfc43b2c81775138dd454f245e021243
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 19:28:11 +0100

    feat(ordering): add GetOrderByCustomer query and handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByCustomer/GetOrderByCustomerHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrderByCustomer;
+
+public class GetOrderByCustomerHandler(IAppDbContext dbContext)
+    : IQueryHandler<GetOrderByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrderByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        // get orders by customer using dbcontext
+        // return result
+
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+            .OrderBy(o => o.OrderName.Value)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByCustomer/GetOrderByCustomerQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrderByCustomer;
+
+public record GetOrderByCustomerQuery(Guid CustomerId)
+    : IQuery<GetOrdersByCustomerResult>;
+
+public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
