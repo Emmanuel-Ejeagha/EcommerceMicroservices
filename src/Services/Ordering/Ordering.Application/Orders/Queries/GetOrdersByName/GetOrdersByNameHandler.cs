aa6a382859ab0a03621bc75ae039fa8cb3b8afa1
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-03-27 19:28:40 +0100

    feat(ordering): add GetOrderByName query and handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameHandler(IAppDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+
+        // get orders by name using dbcontext
+        // return result
+
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.OrderName.Value.Contains(query.Name))
+            .OrderBy(o => o.OrderName)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }
+}
