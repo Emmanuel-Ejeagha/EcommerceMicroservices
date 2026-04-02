24f84011417bce3819de8b3bccc14a87429c51a0
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-04-02 15:42:40 +0100

    feat(basket): implement CheckoutBasket endpoint, command, and handler

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Basket.CheckoutBasket;
+
+public class CheckoutBasketCommandValidator
+    : AbstractValidator<CheckoutBasketCommand>
+{
+    public CheckoutBasketCommandValidator()
+    {
+        RuleFor(x => x.BasketCheckoutDto).NotNull().WithMessage("BasketCheckoutDto cannot be null");
+        RuleFor(x => x.BasketCheckoutDto.UserName).NotEmpty().WithMessage("Username is required");
+    }
+}
